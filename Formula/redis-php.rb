d4f20668c8780a1e3246f46b9fbbfe7bdd96462c
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-03-03 23:43:25 -0500

    RedisPHP formula

---
+++ Formula/redis-php.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class RedisPhp < Formula
+  url 'https://github.com/nicolasff/phpredis/tarball/2.1.3'
+  head 'https://github.com/nicolasff/phpredis.git'
+  homepage 'https://github.com/nicolasff/phpredis'
+  md5 'eb2bee7e42f7a32a38c2a45377f21086'
+
+  def install
+    system "phpize"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    To finish installing redis-php:
+      * Add the following line to php.ini:
+        extension="#{prefix}/imagick.so"
+      * Restart your webserver
+      * Write a PHP page that calls "phpinfo();"
+      * Load it in a browser and look for the info on the redis module.
+      * If you see it, you have been successful!
+    EOS
+  end
+end
