3967492947c6040759ddd25a48ba334b421b807f
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-03-04 02:59:25 -0500

    Minor consistency changes to uploadprogress-php extension

--- Formula/uploadprogress-php.rb
+++ Formula/uploadprogress-php.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Uploadprogress < Formula
+class UploadprogressPhp < Formula
   homepage 'http://pecl.php.net/package/uploadprogress'
   url 'http://pecl.php.net/get/uploadprogress-1.0.3.1.tgz'
   md5 '13fdc39d68e131f37c4e18c3f75aeeda'
@@ -18,11 +18,13 @@ class Uploadprogress < Formula
   end
 
   def caveats; <<-EOS.undent
-    To finish installing uploadprogress:
-     * Add the following lines to php.ini:
-        [uploadprogress]
-        extension="#{prefix}/uploadprogress.so"
-     * Restart your webserver
+    To finish installing uploadprogress-php:
+      * Add the following line to #{etc}/php.ini:
+        zend_extension="#{prefix}/uploadprogress.so"
+      * Restart your webserver.
+      * Write a PHP page that calls "phpinfo();"
+      * Load it in a browser and look for the info on the uploadprogress module.
+      * If you see it, you have been successful!
     EOS
   end
 end
