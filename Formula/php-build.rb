ed17cdbf6fe3efd4f61c09c2885a2a9bfa5aa1d8
Luke Gallagher <notfornoone@gmail.com>
2012-03-05 14:58:18 +1100

    Add php-build formula

---
+++ Formula/php-build.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class PhpBuild < Formula
+  homepage 'http://chh.github.com/php-build/'
+  url 'https://github.com/CHH/php-build/zipball/v0.4.0'
+  md5 'dd785277472d21ad1282a6d50efffc55'
+  head 'https://github.com/CHH/php-build.git'
+
+  def install
+    bin.install Dir['bin/php-build']
+    share.install Dir['share/php-build']
+    man1.install Dir['man/php-build.1']
+  end
+
+  def test
+    system "php-build --definitions"
+  end
+
+  def caveats; <<-EOS.undent
+    Tidy is enabled by default which will only work
+    on 10.7. Be sure to disable or patch Tidy for
+    earlier versions of OS X.
+    EOS
+  end
+end
