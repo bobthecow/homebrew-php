3eab3c541241e4067e306d4c3a504dbce73d35dd
Andy Blyler <andy@blyler.cc>
2012-03-06 11:19:44 -0800

    add head version of memcached-php

--- Formula/memcached-php.rb
+++ Formula/memcached-php.rb
@@ -4,17 +4,19 @@ class MemcachedPhp < Formula
   homepage 'http://pecl.php.net/package/memcached'
   url 'http://pecl.php.net/get/memcached-2.0.1.tgz'
   md5 'f81a5261be1c9848ed5c071a4ebe5e05'
+  head 'https://github.com/php-memcached-dev/php-memcached.git'
 
   depends_on 'libmemcached'
 
   def install
-    Dir.chdir "memcached-#{version}" do
-      system "phpize"
-      system "./configure", "--prefix=#{prefix}",
-                            "--with-libmemcached-dir=#{Formula.factory('libmemcached').prefix}"
-      system "make"
-      prefix.install 'modules/memcached.so'
+    if not ARGV.build_head?
+      cd "memcached-#{version}"
     end
+
+    system "phpize"
+    system "./configure", "--prefix=#{prefix}", "--with-libmemcached-dir=#{Formula.factory('libmemcached').prefix}"
+    system "make"
+    prefix.install 'modules/memcached.so'
   end
 
   def caveats; <<-EOS.undent
