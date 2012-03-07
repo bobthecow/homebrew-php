60d8210c1985981feffbb3dc094bddf266b60eca
Arnaud Lafon <arnaud.lafon@gmail.com>
2012-03-07 12:11:24 +0100

    Added new formula (intl for php) Initially added to @mxcl repository at https://github.com/mxcl/homebrew/pull/9702#issuecomment-4350159 PHP Version is 5.3.10

---
+++ Formula/intl-php.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class IntlPhp < Formula
+  homepage 'http://fr2.php.net/manual/en/book.intl.php'
+  url 'http://www.php.net/get/php-5.3.10.tar.bz2/from/this/mirror'
+  md5 '816259e5ca7d0a7e943e56a3bb32b17f'
+  version '5.3.10'
+
+  depends_on 'icu4c'
+
+  def install
+    Dir.chdir "ext/intl"
+    system "phpize"
+    system "./configure", "--enable-intl",
+                          "--prefix=#{prefix}"
+    system "make"
+    prefix.install 'modules/intl.so'
+  end
+
+  def caveats; <<-EOS.undent
+    To finish intl-php installation, you need to add the
+    following line into #{etc}/php.ini:
+
+      extension=#{prefix}/intl.so
+
+    Then, restart your webserver and check in phpinfo if
+    you're able to see something about intl
+    EOS
+  end
+end
