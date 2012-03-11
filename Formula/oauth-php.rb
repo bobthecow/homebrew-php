996f1064cd50af6f841f92077df413f2a78b96cb
Gabriel Evans <gabriel@codeconcoction.com>
2012-03-11 05:36:14 -0700

    Add oauth-php formula

---
+++ Formula/oauth-php.rb
@@ -0,0 +1,32 @@
+require 'formula'
+
+class OauthPhp < Formula
+  homepage 'http://pecl.php.net/package/oauth'
+  url 'http://pecl.php.net/get/oauth-1.2.2.tgz'
+  md5 '9a9f35e45786534d8580abfffc8c273c'
+  head 'https://svn.php.net/repository/pecl/oauth/trunk', :using => :svn
+
+  depends_on 'libyaml'
+
+  def install
+    if not ARGV.build_head?
+      Dir.chdir "oauth-#{version}"
+    end
+
+    system "phpize"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    prefix.install 'modules/oauth.so'
+  end
+
+  def caveats; <<-EOS.undent
+    To finish installing oauth-php:
+      * Add the following line to #{etc}/php.ini:
+        extension="#{prefix}/oauth.so"
+      * Restart your webserver.
+      * Write a PHP page that calls "phpinfo();"
+      * Load it in a browser and look for the info on the oauth module.
+      * If you see it, you have been successful!
+    EOS
+  end
+end
