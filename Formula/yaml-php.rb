fe4f346b80680f63f6359a3c93b0a706fd4b6ea0
Gabriel Evans <gabriel@codeconcoction.com>
2012-03-11 05:36:04 -0700

    Add yaml-php formula

---
+++ Formula/yaml-php.rb
@@ -0,0 +1,32 @@
+require 'formula'
+
+class YamlPhp < Formula
+  homepage 'http://pecl.php.net/package/yaml'
+  url 'http://pecl.php.net/get/yaml-1.0.1.tgz'
+  md5 'd8a965479d919e1526dd43295783c7f7'
+  head 'https://svn.php.net/repository/pecl/yaml/trunk', :using => :svn
+
+  depends_on 'libyaml'
+
+  def install
+    if not ARGV.build_head?
+      Dir.chdir "yaml-#{version}"
+    end
+
+    system "phpize"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    prefix.install 'modules/yaml.so'
+  end
+
+  def caveats; <<-EOS.undent
+    To finish installing yaml-php:
+      * Add the following line to #{etc}/php.ini:
+        extension="#{prefix}/yaml.so"
+      * Restart your webserver.
+      * Write a PHP page that calls "phpinfo();"
+      * Load it in a browser and look for the info on the yaml module.
+      * If you see it, you have been successful!
+    EOS
+  end
+end
