a27d44d0579517c3ae9191679b52ddc099705523
Jake Bell <jake@theunraveler.com>
2012-03-28 19:14:15 -0500

    Adding formula for php-version.

---
+++ Formula/php-version.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class PhpVersion < Formula
+  url 'https://github.com/wilmoore/php-version/tarball/0.7.1'
+  homepage 'https://github.com/wilmoore/php-version'
+  md5 '83bd52b6a674568db314e5f9ecb88ea3'
+
+  head 'https://github.com/wilmoore/php-version.git'
+
+  def install
+    prefix.install Dir['*']
+  end
+
+  def caveats;
+    <<-EOS.undent
+      For Bash, add the following into your $HOME/.bashrc or $HOME/.bash_profile:
+
+        ########################################################################################
+        # php-version (activate default PHP version and autocompletion)
+        # PHP_HOME                      => should reflect location of compiled PHP versions
+        # PHPVERSION_DISABLE_COMPLETE=1 => to disable shell completion
+        ########################################################################################
+        export PHP_HOME=$HOME/local/php/versions
+        source $(brew --prefix php-version)/php-version.sh && php-version 5.4.0 >/dev/null
+    EOS
+  end
+end
