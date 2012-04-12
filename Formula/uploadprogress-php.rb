ac5cfd1ed618d17126b14bd6364a0d1a3996b67c
Alan Ivey <alanivey@gmail.com>
2012-04-12 11:43:21 -0400

    Change zend_extension to extension, because using zend_extension causes PHP to say: "uploadprogress.so doesn't appear to be a valid Zend extension"

--- Formula/uploadprogress-php.rb
+++ Formula/uploadprogress-php.rb
@@ -21,7 +21,7 @@ class UploadprogressPhp < Formula
   def caveats; <<-EOS.undent
     To finish installing uploadprogress-php:
       * Add the following line to #{etc}/php.ini:
-        zend_extension="#{prefix}/uploadprogress.so"
+        extension="#{prefix}/uploadprogress.so"
       * Restart your webserver.
       * Write a PHP page that calls "phpinfo();"
       * Load it in a browser and look for the info on the uploadprogress module.
