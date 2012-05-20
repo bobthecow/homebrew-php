56a85f41e5292691c6e5f27ab4abfde6d923170d
Wil Moore III <wil.moore@wilmoore.com>
2012-05-20 01:33:24 -0600

    php-version 0.7.1 => 0.9.2

--- Formula/php-version.rb
+++ Formula/php-version.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class PhpVersion < Formula
-  homepage 'https://github.com/wilmoore/php-version'
-  url 'https://github.com/wilmoore/php-version/tarball/0.7.1'
-  md5 '83bd52b6a674568db314e5f9ecb88ea3'
-  head 'https://github.com/wilmoore/php-version.git'
+  homepage  'http://wilmoore.github.com/php-version'
+  url       'https://github.com/wilmoore/php-version/tarball/0.9.2'
+  md5       '4fe3d8cacd589d82104d08bd6746a405'
+  head      'https://github.com/wilmoore/php-version.git'
 
   def install
     prefix.install Dir['*']
@@ -12,15 +12,19 @@ class PhpVersion < Formula
 
   def caveats;
     <<-EOS.undent
-      For Bash, add the following into your $HOME/.bashrc or $HOME/.bash_profile:
+      Add the following script block to `$HOME/.bashrc`, `$HOME/.zshrc`, or your shell's equivalent configuration file.
 
-        ########################################################################################
-        # php-version (activate default PHP version and autocompletion)
-        # PHP_HOME                      => should reflect location of compiled PHP versions
-        # PHPVERSION_DISABLE_COMPLETE=1 => to disable shell completion
-        ########################################################################################
-        export PHP_HOME=$HOME/local/php/versions
-        source $(brew --prefix php-version)/php-version.sh && php-version 5.4.0 >/dev/null
+      Change `5.x.x` to the version of PHP you'd like your shell to default to or remove `php-version 5.x.x >/dev/null`
+      if you do not wish to have a default version of PHP loaded into your `$PATH`. The large comment block is optional:
+
+      ########################################################################################
+      # php-version (activate default PHP version and autocompletion)
+      # PHP_HOME                      => should reflect location of compiled PHP versions
+      # PHPVERSION_DISABLE_COMPLETE=1 => to disable shell completion
+      ########################################################################################
+      export PHP_VERSIONS=$(dirname $(brew --prefix php))
+      [ -f $(brew --prefix php-version)/php-version.sh ] &&
+        source $(brew --prefix php-version)/php-version.sh && php-version 5.x.x >/dev/null
     EOS
   end
 end
