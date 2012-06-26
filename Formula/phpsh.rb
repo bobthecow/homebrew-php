b911a24c35a1616500498064a1e388f549048105
Sébastien Lavoie <github@lavoie.sl>
2012-06-26 14:15:13 -0400

    [phpsh] Changed python dependency to comply with brew audit, matched style of redis extension

--- Formula/phpsh.rb
+++ Formula/phpsh.rb
@@ -1,11 +1,15 @@
 require 'formula'
 
+def python_installed?
+  `which python`.length > 0
+end
+
 class Phpsh < Formula
   homepage 'http://www.phpsh.org/'
   url 'https://github.com/facebook/phpsh.git'
   version "1.3"
 
-  depends_on 'python'
+  depends_on 'python' => :recommended unless python_installed?
 
   def install
     system "python","setup.py", "install","--prefix=#{prefix}"
