b9c7c40e044907c13cc41b02479bb347843c4289
Bruno Galeotti <bgaleotti@gmail.com>
2012-07-20 12:48:43 -0300

    Added PHP Coding Standards Fixer

---
+++ Formula/php-cs-fixer.rb
@@ -0,0 +1,39 @@
+require 'formula'
+
+def php_installed?
+    `which php`.length > 0
+end
+
+def php_phar_module_enabled?
+    `php -m`.downcase.include? "phar"
+end
+
+class PhpCsFixer < Formula
+  homepage 'http://cs.sensiolabs.org'
+  url 'http://cs.sensiolabs.org/get/php-cs-fixer.phar'
+  sha1 'd8aee46fa5515513bab100635992c85ad08258c7'
+  version '0.2'
+
+  depends_on 'php53' => :recommended unless php_installed? && php_phar_module_enabled?
+
+  def install
+    libexec.install "php-cs-fixer.phar"
+    sh = libexec + "php-cs-fixer"
+    sh.write("/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/php-cs-fixer.phar $*")
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
+
+  def test
+    system 'php-cs-fixer --version'
+  end
+
+  def caveats; <<-EOS.undent
+    Verify your installation by running:
+      "php-cs-fixer --version".
+
+    You can read more about php-cs-fixer by running:
+      "brew home php-cs-fixer".
+    EOS
+  end
+end
