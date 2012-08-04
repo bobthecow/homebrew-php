41069b0143a79e9c5a5fe1b75ffa10d196ce9991
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-08-04 18:59:53 -0400

    Added head build for xdebug. Closes #203

--- Formula/php54-xdebug.rb
+++ Formula/php54-xdebug.rb
@@ -4,11 +4,12 @@ class Php54Xdebug < Formula
   homepage 'http://xdebug.org'
   url 'http://xdebug.org/files/xdebug-2.2.1.tgz'
   md5 '5e5c467e920240c20f165687d7ac3709'
+  head 'https://github.com/derickr/xdebug.git'
 
   depends_on 'autoconf' => :build
 
   def install
-    Dir.chdir "xdebug-#{version}"
+    Dir.chdir "xdebug-#{version}" unless ARGV.build_head?
 
     # See https://github.com/mxcl/homebrew/issues/issue/69
     ENV.universal_binary
