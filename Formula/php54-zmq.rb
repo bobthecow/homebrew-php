37e088c3eaf791466d9417c2d27d5552111d8df3
Julio Viera <julio.viera@gmail.com>
2012-09-05 11:05:37 -0400

    Adds php-zmq 1.0.3

---
+++ Formula/php54-zmq.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Zmq < AbstractPhpExtension
+  homepage 'http://php.zero.mq/'
+  url 'https://github.com/mkoppanen/php-zmq/tarball/1.0.3'
+  md5 'aca2588f94e365a16a5734f89cb633c8'
+  head 'git://github.com/mkoppanen/php-zmq.git'
+  version '1.0.3'
+
+  depends_on 'autoconf' => :build
+  depends_on 'php54' if ARGV.include?('--with-homebrew-php') && !Formula.factory('php54').installed?
+
+  def install
+    # See https://github.com/mxcl/homebrew/pull/5947
+    ENV.universal_binary
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    prefix.install "modules/zmq.so"
+    write_config_file unless ARGV.include? "--without-config-file"
+  end
+end
