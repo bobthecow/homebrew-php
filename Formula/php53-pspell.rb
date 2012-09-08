e7f1f38aff088fb60f7995d2a7c65de9085f18c0
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-09-08 01:49:32 +0100

    md5 mismatch

--- Formula/php53-pspell.rb
+++ Formula/php53-pspell.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Pspell < AbstractPhpExtension
   homepage 'http://php.net/manual/en/book.pspell.php'
   url 'http://www.php.net/get/php-5.3.16.tar.bz2/from/this/mirror'
-  md5 '370be99c5cdc2e756c82c44d774933c8'
+  md5 '99cfd78531643027f60c900e792d21be'
   version '5.3.16'
 
   depends_on 'aspell'
