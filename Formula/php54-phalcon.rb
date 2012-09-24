add4f05db1be8f7e7e85d495a48a8b7c4266920e
hfcorriez <hfcorriez@gmail.com>
2012-09-24 08:57:52 +0800

    Add phalcon extension framework.

---
+++ Formula/php54-phalcon.rb
@@ -0,0 +1,30 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Phalcon < AbstractPhpExtension
+  homepage 'http://phalconphp.com/'
+  url 'https://github.com/phalcon/cphalcon/tarball/v0.4.5'
+  version '0.4.5'
+  sha1 'e941f84eb236492507245b539bec7cdb69def14a'
+  head 'git://github.com/phalcon/cphalcon.git', :using => :git
+
+  depends_on 'autoconf' => :build
+  depends_on 'php54' if build.include?('with-homebrew-php') && !Formula.factory('php54').installed?
+  depends_on 'pcre'
+
+  def install
+    if build.head?
+      Dir.chdir "build"
+    else
+      Dir.chdir "release"
+    end
+
+    ENV.universal_binary
+
+    safe_phpize
+    system 'export CFLAGS="-O2 -fno-delete-null-pointer-checks"'
+    system "./configure", "--prefix=#{prefix}", "--enable-phalcon"
+    system "make"
+    prefix.install "modules/phalcon.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
