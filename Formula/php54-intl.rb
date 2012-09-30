4372d1bbd1fd23a16e8689463312935114366e5d
tiraeth <tiraeth@gmail.com>
2012-09-30 20:48:40 +0200

    Fixed extensions relying on PHP 5.4 source tarball after updating formulae to 5.4.7

--- Formula/php54-intl.rb
+++ Formula/php54-intl.rb
@@ -2,9 +2,9 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 
 class Php54Intl < AbstractPhpExtension
   homepage 'http://php.net/manual/en/book.intl.php'
-  url 'http://www.php.net/get/php-5.4.6.tar.bz2/from/this/mirror'
-  sha1 'a092ff9632f96189ade4415ca1e0a89cbc289a4b'
-  version '5.4.6'
+  url 'http://www.php.net/get/php-5.4.7.tar.bz2/from/this/mirror'
+  sha1 'e634fbbb63818438636bf83a5f6ea887d4569943'
+  version '5.4.7'
 
   depends_on 'autoconf' => :build
   depends_on 'icu4c'
--- Formula/php54-pspell.rb
+++ Formula/php54-pspell.rb
@@ -2,9 +2,9 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 
 class Php54Pspell < AbstractPhpExtension
   homepage 'http://php.net/manual/en/book.pspell.php'
-  url 'http://www.php.net/get/php-5.4.6.tar.bz2/from/this/mirror'
-  sha1 'a092ff9632f96189ade4415ca1e0a89cbc289a4b'
-  version '5.4.6'
+  url 'http://www.php.net/get/php-5.4.7.tar.bz2/from/this/mirror'
+  sha1 'e634fbbb63818438636bf83a5f6ea887d4569943'
+  version '5.4.7'
 
   depends_on 'aspell'
   depends_on 'autoconf' => :build
