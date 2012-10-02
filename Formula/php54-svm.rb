1670278c107dc18c427a14fef163164d0016e295
tiraeth <tiraeth@gmail.com>
2012-10-02 09:34:21 +0200

    Added SVM extension for 5.3 and 5.4

---
+++ Formula/php54-svm.rb
@@ -0,0 +1,26 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Svm < AbstractPhp54Extension
+  homepage 'http://php.net/manual/en/book.svm.php'
+  url 'https://github.com/ianbarber/php-svm/tarball/master'
+  sha1 '10b3a64170639602f9929c1b47d1c4f4da210a2b'
+  head 'https://github.com/ianbarber/php-svm.git'
+  version '0.1.8'
+
+  depends_on 'autoconf' => :build
+  depends_on 'php54' if build.include?('with-homebrew-php') && !Formula.factory('php54').installed?
+
+  def install
+    Dir.chdir "svm-#{version}" unless build.head?
+
+    # See https://github.com/mxcl/homebrew/pull/5947
+    ENV.universal_binary
+
+    safe_phpize
+    ENV["CFLAGS"] = '-Wno-return-type'
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    prefix.install "modules/svm.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
