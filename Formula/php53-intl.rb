9ace74c9822b26e79300d26fcc5adc53fde4b31e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-10-07 16:07:57 -0400

    Changed download mirror

--- Formula/php53-intl.rb
+++ Formula/php53-intl.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 
 class Php53Intl < AbstractPhp53Extension
   homepage 'http://php.net/manual/en/book.intl.php'
-  url 'http://www.php.net/get/php-5.3.17.tar.bz2/from/a/mirror'
+  url 'http://www.php.net/get/php-5.3.17.tar.bz2/from/this/mirror'
   sha1 'd6f0192d2c1dae2921923762bde5ae356ceda5b5'
   version '5.3.17'
 
