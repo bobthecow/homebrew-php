68cc0c2f8cb298689ab78cfe96181c37e6e8faa6
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-11-07 22:16:03 -0500

    Upgraded php53-imagick to the rc

--- Formula/php53-imagick.rb
+++ Formula/php53-imagick.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 
 class Php53Imagick < AbstractPhp53Extension
   homepage 'http://pecl.php.net/package/imagick'
-  url 'http://pecl.php.net/get/imagick-3.0.1.tgz'
+  url 'http://pecl.php.net/get/imagick-3.1.0RC2.tgz'
   sha1 '29b6dcd534cde6b37ebe3ee5077b71a9eed685c2'
   head 'https://svn.php.net/repository/pecl/imagick/trunk/', :using => :svn
 
