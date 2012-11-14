86e9a05d93f300f30e8ec3a967669050244721b8
Romain Neutron <imprec@gmail.com>
2012-11-15 00:17:34 +0100

    Add php libevent extension formulae for PHP 5.3 and 5.4

---
+++ Formula/php54-libevent.rb
@@ -0,0 +1,26 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Libevent < AbstractPhp54Extension
+  init
+  homepage 'http://pecl.php.net/package/libevent'
+  url 'http://pecl.php.net/get/libevent-0.0.5.tgz'
+  sha1 '0817616ef02a5ab2bbc804dad121239391578956'
+  version '0.0.5'
+
+  depends_on 'libevent'
+
+  def install
+    Dir.chdir "libevent-#{version}" unless build.head?
+
+    # See https://github.com/mxcl/homebrew/pull/5947
+    ENV.universal_binary
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-libevent=#{Formula.factory('libevent').prefix}"
+    system "make"
+    prefix.install "modules/libevent.so"
+    write_config_file unless build.include? "without-config-file"
+  end
+end
