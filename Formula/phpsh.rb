346d3a0a5653296fec18ae212cca128f59896308
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-11-14 10:01:06 -0500

    Require python in all cases, fixing failed installations due to superenv changes

--- Formula/phpsh.rb
+++ Formula/phpsh.rb
@@ -1,15 +1,11 @@
 require 'formula'
 
-def python_installed?
-  `which python`.length > 0
-end
-
 class Phpsh < Formula
   homepage 'http://www.phpsh.org/'
   url 'https://github.com/facebook/phpsh.git'
   version "1.3"
 
-  depends_on 'python' => :recommended unless python_installed?
+  depends_on 'python'
 
   def install
     system "python","setup.py", "install","--prefix=#{prefix}"
