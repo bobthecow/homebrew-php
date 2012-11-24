415264b3f412e220af58761a8e236f13a82808b5
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-11-24 05:07:58 -0500

    Build php-cs-fixer.phar on the fly. Closes #377
    @fabpot refuses to release tagged builds for php-cs-fixer because it is
    pre-alpha software. As such, it is impossible to follow his daily rebuilds
    of the released phar, making it a chore to maintain php-cs-fixer.
    
    This commit retools the work by @davidneimeyer in his php-cs-fixer_installer
    repo into a homebrew formulae, allowing us to build the phar file on the fly.
    Because of the phar requirements, phar.readonly must be set to off for This
    formulae to run properly.
    
    Note that detection of installed php bins may fail if you are using HEAD for
    your homebrew-php install.

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -1,17 +1,37 @@
 require 'formula'
 require File.join(HOMEBREW_LIBRARY, 'Taps', 'josegonzalez-php', 'Requirements', 'php-meta-requirement')
 require File.join(HOMEBREW_LIBRARY, 'Taps', 'josegonzalez-php', 'Requirements', 'phar-requirement')
+require File.join(HOMEBREW_LIBRARY, 'Taps', 'josegonzalez-php', 'Requirements', 'phar-building-requirement')
 
 class PhpCsFixer < Formula
   homepage 'http://cs.sensiolabs.org'
-  url 'http://cs.sensiolabs.org/get/php-cs-fixer.phar'
-  sha1 'c93538d937952226e1e7184c6fc0ef8fe778df62'
-  version '0.2'
+  url 'https://github.com/fabpot/PHP-CS-Fixer/archive/v0.1.0.tar.gz'
+  sha1 '3d25398022d97bf62663d0fe274104a94bca67ef'
+  version '0.1.0'
+  head 'https://github.com/fabpot/PHP-CS-Fixer.git'
 
-  depends_on PhpMetaRequirement.new
-  depends_on PharRequirement.new
+  def self.init
+    depends_on PhpMetaRequirement.new
+    depends_on PharRequirement.new
+    depends_on PharBuildingRequirement.new
+    depends_on "composer"
+    depends_on "php53" if Formula.factory("php53").installed?
+    depends_on "php54" if Formula.factory("php54").installed?
+  end
+
+  init
 
   def install
+    File.open("genphar.php", 'w') {|f| f.write(phar_stub) }
+
+    cmd = [
+      "mkdir -p src",
+      "rsync -a --exclude 'src' . src/",
+      "cd src && /usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off  #{Formula.factory('composer').libexec}/composer.phar install",
+      "cd src && sed -i '' '1d' php-cs-fixer",
+      "php -f genphar.php",
+    ].each { |c| `#{c}` }
+
     libexec.install "php-cs-fixer.phar"
     sh = libexec + "php-cs-fixer"
     sh.write("/usr/bin/env php -d allow_url_fopen=On -d detect_unicode=Off #{libexec}/php-cs-fixer.phar $*")
@@ -21,6 +41,21 @@ class PhpCsFixer < Formula
 
   def test
     system 'php-cs-fixer --version'
+  end
+
+  def phar_stub
+    <<-EOS.undent
+      <?php
+      $stub =<<<STUB
+      <?php
+      /** This was auto-built from source (https://github.com/fabpot/PHP-CS-Fixer) via Homebrew **/
+      Phar::mapPhar('php-cs-fixer.phar'); require 'phar://php-cs-fixer.phar/php-cs-fixer'; __HALT_COMPILER(); ?>";
+      STUB;
+      $phar = new Phar('php-cs-fixer.phar');
+      $phar->setAlias('php-cs-fixer.phar');
+      $phar->buildFromDirectory('src');
+      $phar->setStub($stub);
+    EOS
   end
 
   def caveats; <<-EOS.undent
