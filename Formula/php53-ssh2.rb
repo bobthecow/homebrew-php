135033a13eaebb8dd61b3953aa37aaeb7fd2366d
Daniel Costa <danielcosta@gmail.com>
2012-12-26 18:23:31 -0200

    updating ssh2 from pecl

--- Formula/php53-ssh2.rb
+++ Formula/php53-ssh2.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php53Ssh2 < AbstractPhp53Extension
   init
   homepage 'http://pecl.php.net/package/ssh2'
-  url 'http://pecl.php.net/get/ssh2-0.11.3.tgz'
-  sha1 '8d1e4b59e1fff368c5a3dde04fc93c5361203077'
+  url 'http://pecl.php.net/get/ssh2-0.12.tgz'
+  sha1 'b86a25bdd3f3558bbcaaa6d876309fbbb5ae134d'
   head 'https://svn.php.net/repository/pecl/ssh2/trunk/', :using => :svn
 
   depends_on 'libssh2'
