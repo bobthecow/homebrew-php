818a1ff0a4447fc9f5d33c03a24513c2665e270c
Daniel Costa <danielcosta@gmail.com>
2012-12-27 16:39:21 -0200

    Updating mongo to 1.3.2

--- Formula/php54-mongo.rb
+++ Formula/php54-mongo.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Mongo < AbstractPhp54Extension
   init
   homepage 'http://pecl.php.net/package/mongo'
-  url 'http://pecl.php.net/get/mongo-1.3.1.tgz'
-  sha1 '2623ea786a3e409b17554464ee8d74c57cb28691'
+  url 'http://pecl.php.net/get/mongo-1.3.2.tgz'
+  sha1 '0c9b780271c47eba6c04303383bdf4918320a835'
   head 'https://github.com/mongodb/mongo-php-driver.git'
 
   def install
