82bc67c84fb0e271157a052c767b9eb42558cdb7
Romain Neutron <imprec@gmail.com>
2012-12-31 12:28:29 +0100

    Update php-zmq to version 1.0.5

--- Formula/php54-zmq.rb
+++ Formula/php54-zmq.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php54Zmq < AbstractPhp54Extension
   init
   homepage 'http://php.zero.mq/'
-  url 'https://github.com/mkoppanen/php-zmq/tarball/1.0.3'
-  sha1 'ac3f28fdad28543ea3280c2a4b73f52c62deaadc'
+  url 'https://github.com/mkoppanen/php-zmq/tarball/1.0.5'
+  sha1 'fae6a746bb581a8e44dbc0ba6c9d79e42bf1d361'
   head 'https://github.com/mkoppanen/php-zmq.git'
 
   depends_on 'pkg-config'
