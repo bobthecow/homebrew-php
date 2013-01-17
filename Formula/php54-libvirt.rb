11c0ff7d442e2c0ec02d35c036b7c95d8262ac4a
Andy Blyler <andy@blyler.cc>
2013-01-17 15:31:32 -0500

    add libvirt 0.4.7 extention

---
+++ Formula/php54-libvirt.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php54Libvirt < AbstractPhp54Extension
+  init
+  homepage 'http://libvirt.org/php'
+  url 'git://libvirt.org/libvirt-php', :using => :git, :tag => 'libvirt-php-0.4.7'
+  version '0.4.7'
+
+  depends_on 'automake' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'libvirt'
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    # autogen
+    system "PATH=$PATH:/usr/local/bin ./autogen.sh"
+
+    # safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          "--with-libvirt=#{Formula.factory('libvirt').prefix}"
+    system "make"
+    prefix.install 'src/libvirt-php.so' => 'libvirt.so'
+    write_config_file unless build.include? "without-config-file"
+  end
+end
